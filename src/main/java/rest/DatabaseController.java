7344468a8973102be728b23d24c0884949a49323
Liubomyr Piadyk <liubomyr.piadyk@gmail.com>
2018-11-27 01:41:05 +0200

    update rest and task.txt

--- src/main/java/rest/DatabaseController.java
+++ src/main/java/rest/DatabaseController.java
@@ -3,10 +3,10 @@ package rest;
 import database.Database;
 import database.DatabaseReader;
 import database.Result;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class DatabaseController {
@@ -22,49 +22,50 @@ public class DatabaseController {
         }
     }
 
-    @RequestMapping(value = "/tables", method = RequestMethod.GET)
+    @GetMapping(value = "/database/tables")
     public Result tables() {
         return database.query("list tables");
     }
 
-    @RequestMapping(value = "/drop_table", method = RequestMethod.DELETE)
-    public Result dropTable(@RequestParam(name = "table") String tableName) {
+    @DeleteMapping(value = "/database/tables/delete/{tableName}")
+    public Result dropTable(@PathVariable String tableName) {
         return database.query(String.format("drop table %s", tableName));
     }
 
-    @RequestMapping(value = "/create_table", method = RequestMethod.POST)
-    public Result createTable(@RequestParam(name = "columns") String columns,
-                              @RequestParam(name = "table") String tableName) {
+    @PostMapping(value = "/database/tables/create/{tableName}/{columns}")
+    public Result createTable(@PathVariable String columns,
+                              @PathVariable String tableName) {
         return database.query(String.format("create table %s (%s)", tableName, columns));
     }
 
-    @RequestMapping(value = "/select", method = RequestMethod.GET)
-    public Result createTable(@RequestParam(name = "columns") String columns,
-                              @RequestParam(name = "table") String tableName,
-                              @RequestParam(name = "condition", defaultValue = "") String condition) {
-        String query = String.format("select %s from %s", columns, tableName, columns);
-        if (!condition.isEmpty()) query += String.format(" where %s", condition);
-        return database.query(query);
+    @GetMapping(value = "/database/{tableName}/select/{columns}/{condition}")
+    public Result selectCondition(@PathVariable String columns,
+                                  @PathVariable String tableName,
+                                  @PathVariable String condition) {
+        return database.query(String.format("select %s from %s where %s", columns, tableName, condition));
     }
 
-    @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    public Result insert(@RequestParam(name = "columns") String columns,
-                         @RequestParam(name = "table") String tableName,
-                         @RequestParam(name = "values") String values) {
-        String query = String.format("insert into %s (%s) values (%s)", tableName, columns, values);
-        return database.query(query);
+    @GetMapping(value = "/database/{tableName}/select/{columns}")
+    public Result select(@PathVariable String columns,
+                         @PathVariable String tableName) {
+        return database.query(String.format("select %s from %s", columns, tableName));
     }
 
-    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
-    public Result delete(@RequestParam(name = "table") String tableName,
-                         @RequestParam(name = "condition") String condition) {
-        String query = String.format("delete from %s where %s", tableName, condition);
-        return database.query(query);
+    @PostMapping(value = "/database/{tableName}/insert/{columns}/{values}")
+    public Result insert(@PathVariable String columns,
+                         @PathVariable String tableName,
+                         @PathVariable String values) {
+        return database.query(String.format("insert into %s (%s) values (%s)", tableName, columns, values));
     }
 
-    @RequestMapping(value = "/delete_duplicates", method = RequestMethod.DELETE)
-    public Result deleteDuplicates(@RequestParam(name = "table") String tableName) {
-        String query = String.format("delete duplicates %s", tableName);
-        return database.query(query);
+    @DeleteMapping(value = "/database/{tableName}/delete/{condition}")
+    public Result delete(@PathVariable String tableName,
+                         @PathVariable String condition) {
+        return database.query(String.format("delete from %s where %s", tableName, condition));
+    }
+
+    @DeleteMapping(value = "/database/{tableName}/delete_dublicates")
+    public Result deleteDuplicates(@PathVariable String tableName) {
+        return database.query(String.format("delete duplicates %s", tableName));
     }
 }
